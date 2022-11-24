a66de21268a8a714abc4635c8a4b3fd31d5af583
whypepper <92381152+whypepper@users.noreply.github.com>
2022-11-24 10:10:39 +0800

    Add files via upload

--- SimpleCPU/lib/regfile.v
+++ SimpleCPU/lib/regfile.v
@@ -6,9 +6,17 @@ module regfile(
     input wire [4:0] raddr2,
     output wire [31:0] rdata2,
     
-    input wire we,
-    input wire [4:0] waddr,
-    input wire [31:0] wdata
+    input wire wb_we,
+    input wire [4:0] wb_waddr,
+    input wire [31:0] wb_wdata,
+
+    input wire ex_we,
+    input wire [4:0] ex_waddr,
+    input wire [31:0] ex_wdata,
+
+    input wire mem_we,
+    input wire [4:0] mem_waddr,
+    input wire [31:0] mem_wdata
 );
     reg [31:0] reg_array [31:0];
     // write
@@ -19,8 +27,16 @@ module regfile(
     end
 
     // read out 1
-    assign rdata1 = (raddr1 == 5'b0) ? 32'b0 : reg_array[raddr1];
+    assign rdata1 = (raddr1 == 5'b0) ? 32'b0 : 
+                    ((raddr1 == ex_waddr) && ex_we))? ex_wdata:
+                    ((raddr1 == mem_waddr) && mem_we))? mem_wdata:
+                    ((raddr1 == wb_waddr) && wb_we))? wb_wdata:
+                    reg_array[raddr1];
 
     // read out2
-    assign rdata2 = (raddr2 == 5'b0) ? 32'b0 : reg_array[raddr2];
+    assign rdata2 = (raddr2 == 5'b0) ? 32'b0 : 
+                    ((raddr2 == ex_waddr) && ex_we))? ex_wdata:
+                    ((raddr2 == mem_waddr) && mem_we))? mem_wdata:
+                    ((raddr2 == wb_waddr) && wb_we))? wb_wdata:
+                    reg_array[raddr2];
 endmodule
